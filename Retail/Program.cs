140695bdf29ff038d4afd32bed3d44fc111ac9be
linodht <linodh.thayyil@aemo.com.au>
2023-12-20 17:50:43 +1100

    logging added

--- Retail/Program.cs
+++ Retail/Program.cs
@@ -4,55 +4,74 @@ using Retail.AutoMapper;
 using Retail.Model;
 using Retail.Repository;
 using Retail.Utils;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ErrorModel).Assembly));
-
-builder.Services.AddApiVersioning(o =>
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
+try
 {
-    o.AssumeDefaultVersionWhenUnspecified = true;
-    o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
-    o.ReportApiVersions = true;
-    o.ApiVersionReader = ApiVersionReader.Combine(
-        //new QueryStringApiVersionReader("x-api-version"),
-        new HeaderApiVersionReader("x-api-Version"),
-        new MediaTypeApiVersionReader("ver"));
-}).AddVersionedApiExplorer(options =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ErrorModel).Assembly));
+
+    builder.Services.AddApiVersioning(o =>
+    {
+        o.AssumeDefaultVersionWhenUnspecified = true;
+        o.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+        o.ReportApiVersions = true;
+        o.ApiVersionReader = ApiVersionReader.Combine(
+            //new QueryStringApiVersionReader("x-api-version"),
+            new HeaderApiVersionReader("x-api-Version"),
+            new MediaTypeApiVersionReader("ver"));
+    }).AddVersionedApiExplorer(options =>
     {
         options.GroupNameFormat = "'v'VVV";
         options.SubstituteApiVersionInUrl = true;
     });
-builder.Services.AddDbContext<RetailDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionString")));
+    builder.Services.AddDbContext<RetailDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionString")));
 
 
-builder.Services.AddScoped<RepoUow>();
+    builder.Services.AddScoped<RepoUow>();
 
-builder.Services.AddAutoMapperSetup();
+    builder.Services.AddAutoMapperSetup();
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
     app.UseSwagger();
     app.UseSwaggerUI();
 
+    //}
+
+    app.DbMigrate();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.DbMigrate();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
